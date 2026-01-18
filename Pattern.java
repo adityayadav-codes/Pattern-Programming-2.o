ae31b4044e754939d41e5897ffd3dd1d6ddd857f
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-18 22:58:08 +0530

    pattern 10

--- Pattern.java
+++ Pattern.java
@@ -8,7 +8,6 @@ public class Pattern {
             System.out.println();
         }
     }
-
     static void pattern2() {
         System.out.println("Pattern 2:");
         for (int i = 1; i <= 5; i++) {
@@ -18,7 +17,6 @@ public class Pattern {
             System.out.println();
         }
     }
-
     static void pattern3() {
         System.out.println("Pattern 3:");
         for (int i = 1; i <= 5; i++){
@@ -27,8 +25,7 @@ public class Pattern {
             }
             System.out.println();
         }
-    }
-   
+    }  
     static void pattern4() {
         System.out.println("Pattern 4:");
        for (int i = 5; i >= 1; i--) {
@@ -37,8 +34,7 @@ public class Pattern {
             }
             System.out.println();
        }
-    }
-    
+    }    
     static void pattern5() {
         System.out.println("pattern 5:");
         for(int i = 5; i >= 1; i--){
@@ -49,7 +45,6 @@ public class Pattern {
         }
 
     }
-   
     static void pattern6() {
         System.out.println("pattern 6:");
         int k = 1;
@@ -84,6 +79,24 @@ public class Pattern {
             System.out.println();
         }
     }   
+    static void pattern9() {
+        System.out.println("pattern 9 :"); 
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= 5; j++){
+                System.out.printf("%3d ", (i*j));
+            }
+            System.out.println();
+        }
+    }
+    static void pattern10() {
+        System.out.println("pattern 10:");
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= 3; j++) {
+                System.out.printf("%2d %2d ", j, i);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
 
        // pattern1();
@@ -93,7 +106,9 @@ public class Pattern {
         //pattern5();
         //pattern6();
        // pattern7();
-       pattern8();
+       //pattern8();
+       //  pattern9();
+        pattern10();
     }
 
 }
